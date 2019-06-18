1970c77ad8e761fb95df325613be610591f5230d
Spark M <sparkmythought@gmail.com>
2019-06-18 01:42:43 -0700

    Adding fill-in-the-blanks for students to fill

--- sparky-personal-info/__init__.py
+++ sparky-personal-info/__init__.py
@@ -22,30 +22,27 @@ class PersonalSkill(MycroftSkill):
 
     @intent_file_handler("WhenWereYouBorn.intent")
     def handle_when_were_you_born_intent(self, message):
-        self.speak_dialog("when.was.i.born")
+	pass
 
     @intent_file_handler("WhereWereYouBorn.intent")
     def handle_where_were_you_born_intent(self, message):
-        self.speak_dialog("where.was.i.born")
+	pass
 
     @intent_file_handler("WhoMadeYou.intent")
     def handle_who_made_you_intent(self, message):
-        self.speak_dialog("who.made.me")
+	pass
 
     @intent_file_handler("WhoAreYou.intent")
     def handle_who_are_you_intent(self, message):
-        name = self.config_core.get("listener", {}).get("wake_word",
-                                                        "mycroft")
-        name = name.lower().replace("hey ", "")
-        self.speak_dialog("who.am.i", {"name": name})
+        pass
 
     @intent_file_handler("WhatAreYou.intent")
     def handle_what_are_you_intent(self, message):
-        self.speak_dialog("what.am.i")
+	pass
 
     @intent_file_handler("DoYouRhyme.intent")
     def handle_do_you_rhyme(self, message):
-        self.speak_dialog("tell.a.rhyme")
+	pass
 
 
 def create_skill():
