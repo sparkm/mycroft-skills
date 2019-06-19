e613d35b7f4a955e9554dd94e4fc2e40904f0e93
Spark M <sparkmythought@gmail.com>
2019-06-19 07:11:59 -0700

    Adding fill-in-the-blanks for students to code

--- sparky-number-guess/__init__.py
+++ sparky-number-guess/__init__.py
@@ -29,34 +29,11 @@ class NumberGuessSkill(MycroftSkill):
 	userGuess = 0
 
 	def get_numerical_response(self, dialog):
-		while True:
-			val = self.get_response(dialog)
-			try:
-				val = int(val)
-				return val
-			except ValueError:
-				self.speak_dialog("invalid.input")
-			except:
-				self.speak_dialog("input.error")
+		pass
 
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
-		self.speak_dialog("start.game")
-
-		# get lower bound
-		lowerBound = self.get_numerical_response("get.lower")
-		# get upper bound
-		upperBound = self.get_numerical_response("get.upper")
-
-		answer = randint(lowerBound, upperBound)
-		userGuess = lowerBound - 1
-		while userGuess != answer:
-			userGuess = self.get_numerical_response("guess")
-			if userGuess < answer:
-				self.speak_dialog("too.low")
-			elif userGuess > answer:
-				self.speak_dialog("too.high")
-		self.speak_dialog("correct")
+		pass
 
 	def stop(self):
 		lowerBound, upperBound = 0, 100
